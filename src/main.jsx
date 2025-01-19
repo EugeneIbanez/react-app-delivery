4b5547ae9a2610c7e4331b4a425d7008dc3f2507
EugeneIbanez <30960812+EugeneIbanez@users.noreply.github.com>
2025-01-19 11:44:07 +0300

    added .id

--- src/main.jsx
+++ src/main.jsx
@@ -45,18 +45,18 @@ const reactRoot = createRoot(rootElement);
 reactRoot.render(
   <div>
     {restaurants.map((item) => (
-      <div key={item}>
+      <div key={item.id}>
         <h2>{item.name}</h2>
         <h3>Меню</h3>
         <ul>
           {item.menu.map((dish) => (
-            <li key={dish}>{dish.name}</li>
+            <li key={dish.id}>{dish.name}</li>
           ))}
         </ul>
         <h3>Отзывы</h3>
         <ul>
           {item.reviews.map((review) => (
-            <li key={review}>
+            <li key={review.id}>
               <span>{review.user}: </span>
               <span>{review.text}</span>
             </li>
